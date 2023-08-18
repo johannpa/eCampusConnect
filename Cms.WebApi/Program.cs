7e8f7f3595ad2def77012c75c814f123807b45a8
johannpa <johann_paque@hotmail.fr>
2023-08-18 19:42:20 +0200

    Add the class model for the course

--- Cms.WebApi/Program.cs
+++ Cms.WebApi/Program.cs
@@ -9,3 +9,12 @@ app.MapGet("/courses", () =>
 });
 
 app.Run();
+
+
+public class Course
+{
+    public int CourseId { get; set; }
+    public string CourseName { get; set; } = string.Empty;
+    public int CourseDuration { get; set; }
+    public int CourseType { get; set; }
+}
