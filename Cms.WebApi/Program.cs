94ed9b5489c3cd4f6f74573c9765ea5caaa9430a
johannpa <johann_paque@hotmail.fr>
2023-08-18 20:06:48 +0200

    Add the database context and two nugets entity framework core and entity framework in memory

--- Cms.WebApi/Program.cs
+++ Cms.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -10,11 +12,23 @@ app.MapGet("/courses", () =>
 
 app.Run();
 
-
 public class Course
 {
     public int CourseId { get; set; }
     public string CourseName { get; set; } = string.Empty;
     public int CourseDuration { get; set; }
     public int CourseType { get; set; }
+}
+
+public class CmsDatabaseContext: DbContext
+{
+    // It's working with that too
+    //public DbSet<Course> Courses { get; set; }
+
+    //Make this to avoid the null, this is a default Set
+    public DbSet<Course> Courses => Set<Course>();
+    public CmsDatabaseContext(DbContextOptions options) : base(options)
+    {
+        
+    }
 }
