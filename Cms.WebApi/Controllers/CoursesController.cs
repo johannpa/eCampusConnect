57446cf686597a57faa4b49759374c392daa4746
johannpa <johann_paque@hotmail.fr>
2023-09-09 22:31:19 +0200

    Add a new controller

---
+++ Cms.WebApi/Controllers/CoursesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cms.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CoursesController : ControllerBase
+    {
+        public CoursesController()
+        {
+            
+        }
+    }
+}
--- Cms.WebApi/Program.cs
+++ Cms.WebApi/Program.cs
@@ -1,146 +1,25 @@
-using AutoMapper;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Text.Json.Serialization;
-
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<CmsDatabaseContext>(options => 
-  options.UseInMemoryDatabase("CmsDatabase"));
-builder.Services.AddAutoMapper(typeof(CmsMapper));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
-app.MapGet("/courses", async (CmsDatabaseContext db) => 
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    try
-    {
-        var result = await db.Courses.ToListAsync();
-        return Results.Ok(result);
-    }
-    catch (Exception ex)
-    {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-        //throw;
-        return Results.Problem(ex.Message);
-    }
-});
+app.UseHttpsRedirection();
 
-//app.MapPost("/courses", async ([FromBody] CourseDto courseDto, [FromServices] CmsDatabaseContext db, [FromServices] IMapper mapper)
-app.MapPost("/courses", async ( CourseDto courseDto, CmsDatabaseContext db, IMapper mapper) =>
-{
-    try
-    {
-        var newCourse = mapper.Map<Course>(courseDto);
+app.UseAuthorization();
 
-        db.Courses.Add(newCourse);
-        await db.SaveChangesAsync();
-
-        var result = mapper.Map<CourseDto>(newCourse);
-        return Results.Created($"/courses/{result.CourseId}", result);
-    }
-    catch (Exception ex)
-    {
-
-        //throw new InvalidOperationException();
-        //return Results.StatusCode(500);
-        return Results.Problem(ex.Message);
-    }
-});
-
-app.MapGet("/courses/{courseId}", async (int courseId, CmsDatabaseContext db, IMapper mapper) =>
-{
-    var course = await db.Courses.FindAsync(courseId);
-    if(course == null)
-    {
-        return Results.NotFound();
-    }
-
-    var result = mapper.Map<CourseDto>(course); 
-    return Results.Ok(result);
-});
-
-app.MapPut("/courses/{courseId}", async (int courseId, CourseDto courseDto, CmsDatabaseContext db, IMapper mapper) =>
-{
-    var course = await db.Courses.FindAsync(courseId);
-    if (course == null)
-    {
-        return Results.NotFound();
-    }
-
-    course.CourseName = courseDto.CourseName;
-    course.CourseDuration = courseDto.CourseDuration;
-    course.CourseType = (int)courseDto.CourseType;
-    await db.SaveChangesAsync();
-
-    var result = mapper.Map<CourseDto>(course);
-    return Results.Ok(result);
-});
-
-
-app.MapDelete("/courses/{courseId}", async (int courseId, CmsDatabaseContext db, IMapper mapper) =>
-{
-    var course = await db.Courses.FindAsync(courseId);
-    if (course == null)
-    {
-        return Results.NotFound();
-    }
-
-    db.Courses.Remove(course);
-    await db.SaveChangesAsync();
-
-    var result = mapper.Map<CourseDto>(course);
-    return Results.Ok(result);
-});
+app.MapControllers();
 
 app.Run();
-
-
-public class CmsMapper: Profile
-{
-    public CmsMapper()
-    {
-        CreateMap<Course, CourseDto>();
-        CreateMap<CourseDto, Course>();
-    }
-}
-
-public class Course
-{
-    public int CourseId { get; set; }
-    public string CourseName { get; set; } = string.Empty;
-    public int CourseDuration { get; set; }
-    public int CourseType { get; set; }
-}
-
-
-public class CourseDto
-{
-    public int CourseId { get; set; }
-    public string CourseName { get; set; } = string.Empty;
-    public int CourseDuration { get; set; }
-
-    [JsonConverter(typeof(JsonStringEnumConverter))]
-    public COURSE_TYPE CourseType { get; set; }
-}
-
-public enum COURSE_TYPE
-{
-    ENGINEERING = 1,
-    MEDICAL,
-    MANAGMENT
-}
-
-public class CmsDatabaseContext: DbContext
-{
-    // It's working with that too
-    //public DbSet<Course> Courses { get; set; }
-
-    //Make this to avoid the null, this is a default Set
-    public DbSet<Course> Courses => Set<Course>();
-    public CmsDatabaseContext(DbContextOptions options) : base(options)
-    {
-        
-    }
-}
